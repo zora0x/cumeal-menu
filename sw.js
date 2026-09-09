251723000c6a2372f8d385104c947ccd6bb7813e
zora0x <zora0x@users.noreply.github.com>
2026-09-09 21:19:44 +0530

    Add mess timings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cumeal-shell-v1";
+const CACHE_NAME = "cumeal-shell-v2";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
